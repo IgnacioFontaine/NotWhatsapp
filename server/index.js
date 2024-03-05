030e6ca1ebeca4083e11d5502ad16f061d1672fd
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-05 16:24:59 -0300

    socket io instalado y configurado

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const PORT = process.env.PORT ?? 3000;
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+  console.log('A user has connected!');
+})
+
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Esto es el chat</h1>')
+  res.sendFile(process.cwd()+'/client/index.html')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 })
